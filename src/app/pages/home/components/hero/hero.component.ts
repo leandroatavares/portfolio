d64cdfcffec9f633eebec4d758c6f478ef9c3db5
leandroatavares <leandroanjostavares@gmail.com>
2024-07-07 13:00:32 -0300

    feat: :rocket: add social and avaatr image

--- src/app/pages/home/components/hero/hero.component.ts
+++ src/app/pages/home/components/hero/hero.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
-
+  faEnvelope = faEnvelope;
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+  avatar = '/assets/images/leandroatavares.jpg';
 }
