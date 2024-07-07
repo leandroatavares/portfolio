6d652f27c44ff655874833c272595d27d4aa5071
leandroatavares <leandroanjostavares@gmail.com>
2024-07-07 13:52:52 -0300

    style: :rocket: adjust icon for theme switcher

--- src/app/components/actions/actions.component.ts
+++ src/app/components/actions/actions.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-actions',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './actions.component.html',
   styleUrl: './actions.component.scss'
 })
 export class ActionsComponent {
   public isLightTheme = false;
+  faMoon = faMoon;
+  faSun = faSun;
 
   onThemeSwitchChange() {
     this.isLightTheme = !this.isLightTheme;
