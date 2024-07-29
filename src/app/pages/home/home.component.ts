76c3c5ca22c4fe64d9d1ec73930fef0ec5118a5d
leandroatavares <leandroanjostavares@gmail.com>
2024-07-29 11:37:06 -0300

    feat: :sparkles: add contact section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 
 import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ContactComponent } from "./components/contact/contact.component";
 import { HeroComponent } from './components/hero/hero.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, AboutMeComponent, ProjectsComponent],
+  imports: [HeroComponent, AboutMeComponent, ProjectsComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
